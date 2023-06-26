abd8f7ca421a3244f18546ab9362497e546acb9c
Moffinbont <Moffinbont@gmail.com>
2023-06-26 22:10:53 +1200

    Make notes on new lines

--- moffinbont/compainons/summon.js
+++ moffinbont/compainons/summon.js
@@ -91,12 +91,20 @@ CompanionList["summon"] = {
 // >>> Add text to Notes section >>> //
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
 	notes : [{
-		name : "Summon Creature",
-		description : "The creature disappears when it drops to 0 hit points or when the spell ends.",
-		"The creature is an ally to you and your companions.",
-		"In combat, the creature shares your initiative count, but it takes its turn immediately after yours. It obeys your verbal commands (no action required by you). If you don’t issue any, it takes the Dodge action and uses its move to avoid danger.",
-		joinString : "\n   "
-	}, ],
+		name : "The creature disappears when it drops to 0 hit points or when the spell ends.",
+		description : "",
+	joinString : ""
+}, {
+	name : "The creature is an ally to you and your companions.",
+	description : "",
+	joinString : ""
+}, {
+	name : "In combat",
+	description : "the creature shares your initiative count, but it takes its turn immediately after yours. It obeys your verbal commands (no action required by you). If you don’t issue any, it takes the Dodge action and uses its move to avoid danger.",
+joinString : ", "
+},]
+
+
 /*	notes // OPTIONAL //
 	TYPE:	array (variable length) with objects
 	USE:	add text to the leftmost Notes sections on the Companion page
