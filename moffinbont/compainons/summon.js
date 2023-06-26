30d06e363a816fdf55c08d88f0a6eb737ad48750
Moffinbont <Moffinbont@gmail.com>
2023-06-26 22:00:19 +1200

    Finilised Notes Section for all Summons

--- moffinbont/compainons/summon.js
+++ moffinbont/compainons/summon.js
@@ -83,160 +83,20 @@ CompanionList["summon"] = {
 
 	nameMenu : "Summon",
 
-	nameOrigin : "from the spell Summon",
+	nameOrigin : "(TCoE Summon Spell)",
 
 	source : ["TCoE", 109],
-
-	includeCheck : function(sCrea, objCrea, iCreaCR) {
-		return includedNames.contains(objCrea.name);
-		//false; //objCrea.type.toLowerCase() === "beast" && objCrea.size >= 3 && iCreaCR <= 1/4 ? true : false;
-	},
-/*	includeCheck // OPTIONAL //
-	TYPE:	function
-	USE:	filter things from the list of creatures to create the menu options
-
-	This function is called when the Companion Options button is pressed, to generate
-	the menu entries for this companion template option.
-	By default, the only creatures that will be displayed in the menu,
-	will be those that have the `companion` attribute set to this CompanionList object's name.
-	If options should be more dynamic, or if you don't want to alter a bunch of CreatureList entries,
-	it is recommended to add this attribute.
-
-	If what the function returns == `true` for an entry, that entry will be added
-	to the menu options.
-	If the function returns a string, that entry will be added to the menu options and
-	that string will be amended to it (e.g. return " (if DM approves)" for a "Cat" to have it "Cat (if DM approves)" in the menu).
-
-	This function is passed three variables:
-	1)	sCrea
-			A string of the name of the entry in the CreatureList object
-	2)	objCrea
-			The object of the entry (e.g. CreatureList[sCrea])
-	3)	iCreaCR
-			The numerical value of the challenge rating of the CreatureList object
-			(e.g. 0.25 if the challengeRating attribute is "1/4")
-
-	Only creatures in the CreatureList will be processed, not those added through
-	the `creaturesAdd` attribute.
-
-	Note that it is always possible to apply a companion template option to any creature
-	on the Companion Page, using the Companion Options button and selecting to change
-	the current creature into a template option.
-
-	Even though the `companion` attribute of a CreatureList object can be a string for
-	backwards compatibility, the sheet will always correct it and make it into an array
-	to make it easier to check against it using this `includeCheck` function.
-*/
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
-// >>> Change CreatureList object >>> //
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
-/*
-	When a companion template option is selected, the sheet will create a temporary CreatureList object.
-	This object is based on the CreatureList object of the base selected creature (the "Race" dropdown)
-	but will be amended with the settings of the CompanionList object.
-	Together the two create a new object that will be used to populate the Companion Page.
-	You can use the CompanionList object to add, change, or remove attributes of a CreatureList object.
-*/
-	attributesAdd : {
-		header : "Familiar",
-		type : ["Celestial", "Fey", "Fiend"],
-		attacks : [{
-			name : "Acid Spit",
-			ability : 2,
-			damage : [1, 6, "acid"],
-			range : "60 ft",
-			description : ""
-		}],
-	},
-/*	attributesAdd // OPTIONAL //
-	TYPE:	object containing any attributes of the CreatureList object
-	USE:	add or overwrite attributes in the base CreatureList object
-
-	This object can contain almost all attributes of a CreatureList object.
-	See the "creature, wild shape option (CreatureList).js" file for all possible attributes.
-
-	When a companion template option is selected, the sheet will create a temporary CreatureList object
-	that has all the same attributes of the race's CreatureList object, but will amend it with the selected
-	companion template options, if any.
-	The sheet will add any attribute of this `attributesAdd` object to the temporary CreatureList object.
-
-	Using the above example, any creature that has this companion template option selected,
-	will get the header in the top right set to "Familiar", will have its type changed to a choice of
-	"Celestial", "Fey", or "Fiend", and will have the "Acid Spit" attack added to its attack options.
-
-	If the attribute already exists in the base CreatureList object, the attributes will be added together,
-	if they are a string or array. See below how that works:
-
-	TYPE               EFFECT
-	string             the string will be added to the string, with a comma
-	array              the `attributesAdd` array will be added to the end of the CreatureList array
-	boolean, function,
-	number, or object  the `attributesAdd` version will overwrite the CreatureList version
-
-	EXCEPTIONS
-	`attributesAdd` can't contain the following attributes, they will simply not be processed:
-		`name`, `nameAlt`, `companionApply`, `saves`, `scores`, `skills`, `source`,
-		`eval`, `removeeval`, or `changeeval`
-	If `attributesAdd` contains any of the following attributes, they will always overwrite the base:
-		`challengeRating`, `header`, `subtype`, `type`, and `wildshapeString`
-	
-	It is recommended to not include a `calcChanges` this way, but to add it to the main CompanionList object
-	instead, see below at `calcChanges`.
-*/
-	attributesChange : function(sCrea, objCrea) {
-		// can't do any attacks
-		objCrea.attacks = [];
-		if (objCrea.type.toLowerCase() === "beast") {
-			objCrea.type = ["Celestial", "Fey", "Fiend"];
-			objCrea.subtype = "";
-		}
-	},
-/*	attributesChange // OPTIONAL //
-	TYPE:	function
-	USE:	change attributes of the temporary CreatureList object
-
-	After the temporary CreatureList object is generated from the base creature and the `attributesAdd`
-	attribute (see above), that object is run through this function so attributes can be changed dynamically
-
-	Use this to change things that you can't set using `attributesAdd` (see above. Things like the ones that
-	are dependent on other factors (e.g. change subtype only if the base is a certain type),
-	or if you need to change a string or array that you don't want to merge.
-
-	The function is passed two variables:
-	1) sCrea
-		A string of the name of the entry in the CreatureList object
-		You can use this to reference the original base creature, i.e. CreatureList[sCrea]
-		Don't change attributes of this base creature, but instead change the temporary object, objCrea
-	2) objCrea
-		The object that you can change the attributes off so they will be applied on the Companion page
-*/
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
 // >>> Add text to Notes section >>> //
 // >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
 	notes : [{
-		name : "False Appearance",
-		description : "While the purple familiar remains motionless, it is indistinguishable from an ordinary purple flower.",
+		name : "Summon Creature",
+		description : "The creature disappears when it drops to 0 hit points or when the spell ends.",
+		"The creature is an ally to you and your companions.",
+		"In combat, the creature shares your initiative count, but it takes its turn immediately after yours. It obeys your verbal commands (no action required by you). If you don’t issue any, it takes the Dodge action and uses its move to avoid danger.",
 		joinString : "\n   "
-	}, {
-		name : "Invisibility",
-		minlevel : 5,
-		description : "As an action, the purple familiar magically turns invisible until it attacks or casts a spell, or until its concentration ends (as if concentrating on a spell).",
-		addMod : [{ type : "skill", field : "all", mod : "max(oCha|1)", text : "The purple familiar adds its master's Charisma modifier (min 1) to all its skill checks." }]
-	}, {
-		name : "Keen Sight",
-		minlevel : 8,
-		description : "The purple familiar has advantage on Wisdom (Perception) checks that rely on sight. It size increases to Large.",
-		eval : function(prefix, lvl) {
-			// Increase size to Large
-			PickDropdown(prefix + "Comp.Desc.Size", 2);
-		},
-		removeeval : function(prefix, lvl) {
-			// Change size back to Medium
-			PickDropdown(prefix + "Comp.Desc.Size", 3);
-		}
-	}],
+	}, ],
 /*	notes // OPTIONAL //
 	TYPE:	array (variable length) with objects
 	USE:	add text to the leftmost Notes sections on the Companion page
@@ -282,95 +142,6 @@ CompanionList["summon"] = {
 
 	The array is processed in the order it is in the code, no sorting will take place.
 */
-
-/*	minlevel // OPTIONAL //
-	(Part of `notes` object, see above)
-	TYPE:	number
-	USE:	the level at which to add the note
-
-	This attribute is part of an object in the `notes` arrays, see above.
-	Use this if an entry in that array is only supposed to be displayed
-	once the main character (the character on the 1st page) reaches a certain level.
-	If the main character goes below this level, the entry is removed again.
-	The level checked against can be different than the main character level if the
-	attribute `minlevelLinked` exists, see the CreatureList syntax.
-
-	Setting this attribute to 1 is the same as not including it.
-*/
-
-/*	addMod // OPTIONAL //
-	(Part of `notes` object, see above)
-	TYPE:	array of objects (variable length)
-	USE:	add value to a modifier field
-
-	This attribute will only be processed when the `notes` object is processed,
-	which can be influenced using the `minlevel` attribute, see above.
-
-	This attribute works identical to the `addMod` attribute found in the
-	"_common attributes.js" file.
-	Please look there for a complete explanation.
-*/
-
-/*	eval & removeeval // OPTIONAL //
-	(Part of `notes` object, see above)
-	TYPE:	variable, see the entries for `eval` or `removeeval`
-	USE:	variable, see the entries for `eval` or `removeeval`
-
-	These attributes are part of an object in the `notes` arrays, see above.
-	These optional attributes function identical to those that share their name.
-	They function exactly as described for the main object, but they will only be called when the
-	`notes` object is processed, which can be influenced using the `minlevel` attribute, see above.
-*/
-
-
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
-// >>> Change Companion Page calculations >>> //
-// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> //
-	calcChanges : {
-/*	calcChanges // OPTIONAL //
-	TYPE:	object (optional attributes)
-	USE:	change how the hit points automation works
-
-	The attributes of this object can be `hp` and `setAltHp`.
-
-	Note that `calcChanges` also appears in the list of common attributes,
-	but only its `hp` attribute is shared with the object here.
-
-	This object works identical to that present in the CreatureList syntax.
-	You could also add a `calcChanges` object to `attributesAdd`, but if you do that
-	and the CreatureList object has a `calcChanges` as well, only this version in the CompanionList
-	object will be used.
-	By adding a `calcChanges` object to the main CompanionList object, both extra hp calculation options
-	will be added.
-*/
-		hp : function (totalHD, HDobj, prefix) {
-			if (!classes.known.ranger) return;
-			var creaHP = CurrentCompRace[prefix] && CurrentCompRace[prefix].hp ? CurrentCompRace[prefix].hp : 0;
-			var creaName = CurrentCompRace[prefix] && CurrentCompRace[prefix].name ? CurrentCompRace[prefix].name : "the creature";
-			var rngrLvl = classes.known.ranger.level;
-			var rngrCompHp = 4 * rngrLvl;
-			HDobj.alt.push( Math.max(creaHP, rngrCompHp) );
-			HDobj.altStr.push(" = the highest of either\n \u2022 " + creaHp + " from " + creaName + "'s normal maximum HP, or\n \u2022 4 \xD7 " + rngrLvl + " from four times my ranger level (" + rngrCompHp + ")");
-		},
-	/*	hp // OPTIONAL //
-		TYPE:	function
-		USE:	change how Hit Points are calculated and what the Hit Points tooltip says
-
-		This function works identical to the `calcChanges.hp` function found in the
-		"_common attributes.js" file.
-		Please look there for a complete explanation.
-	*/
-		setAltHp : true,
-	/*	setAltHp // OPTIONAL //
-		TYPE:	boolean
-		USE:	set the maximum HP field to automatically assume the alternative calculation method added with the `hp` function
-
-		This attribute will only work if you set the `hp` attribute (see above) in the same object.
-		Set this attribute to true if you push a value to the HDobj.alt array with the function in the `hp` attribute.
-
-		Setting this attribute to false is the same as not including it.
-	*/
-	},
 
 
 // >>>>>>>>>>>>>>>>>>>>>>>>>>> //
