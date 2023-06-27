e745f28badd35107ba2cd65ee609c50c128d1939
Moffinbont <Moffinbont@gmail.com>
2023-06-27 12:33:13 +1200

    This is introduction of aberration and celestial

--- moffinbont/compainons/tasha summon spirits.js
+++ moffinbont/compainons/tasha summon spirits.js
@@ -246,6 +246,286 @@ CreatureList["bestial spirit air"] = {
 	}],
 }
 
+CreatureList["aberrant spirit beholderkin"] = {
+
+	name : "Aberrant Spirit - Beholderkin",
+	source : ["TCoE", 109],
+	size : 3,
+	type : "Aberration",
+	header : "Summon",
+	companion : ["summon"],
+	companionApply : "summon",
+	alignment : "Unaligned",
+
+	ac : 11,
+	hp : 40,
+	hd : [4, 8],
+	speed : "30 ft, fly 30 ft (hover)",
+
+	proficiencyBonus : 0,
+	proficiencyBonusLinked : true,
+	challengeRating : "\"-\"",
+	scores : [16, 10, 15, 16, 10, 6],
+
+	senses : "Darkvision 60 ft",
+	languages : "Deep Speech, understands the languages you speak",
+	damage_immunities : "psychic",
+
+	attacksAction : 2,
+	attacks : [{
+		name : "Eye Ray",
+		ability : 4,
+		useSpellcastingAbility : true,
+		abilitytodamage : true,
+		damage : [1, 8, "psychic"],
+		modifiers : ["", 4],
+		range : "Range 150 ft",
+		description : "Multiattack: Eye Ray number of times equal to half the spell's level"
+	}],
+
+	features : [{
+		name : "Summon Aberration",
+		description : "You call forth an aberrant spirit. It manifests in an unoccupied space that you can see within range. This corporeal form uses the Aberrant Spirit stat block. When you cast the spell, choose a Beholderkin, Slaad, or Star Spawn. The creature resembles an aberration of that kind, which determines certain traits in its stat block.",
+		joinString : "\n   "
+	},{
+		name : "Higher Spell Level",
+		description : "The Aberrant Spirit stat block gains 1 AC, 10 HP, and 1 point of damage on each attack for each spell level above forth",
+		joinString : "\n   "
+	}],
+	actions : [{
+		name : "Multiattack",
+		description : "The aberration makes a number of attacks equal to half this spell's level (rounded down)",
+	}],
+}
+
+CreatureList["aberrant spirit slaad"] = {
+
+	name : "Aberrant Spirit - Slaad",
+	source : ["TCoE", 109],
+	size : 3,
+	type : "Aberration",
+	header : "Summon",
+	companion : ["summon"],
+	companionApply : "summon",
+	alignment : "Unaligned",
+
+	ac : 11,
+	hp : 40,
+	hd : [4, 8],
+	speed : "30 ft",
+
+	proficiencyBonus : 0,
+	proficiencyBonusLinked : true,
+	challengeRating : "\"-\"",
+	scores : [16, 10, 15, 16, 10, 6],
+
+	senses : "Darkvision 60 ft",
+	languages : "Deep Speech, understands the languages you speak",
+	damage_immunities : "psychic",
+
+	attacksAction : 2,
+	attacks : [{
+		name : "Claws",
+		ability : 1,
+		useSpellcastingAbility : true,
+		abilitytodamage : true,
+		damage : [1, 10, "slashing"],
+		modifiers : ["", 4],
+		range : "Range 150 ft",
+		description : "If the target is a creature, it can't regain hit points untill the start of the aberrations next turn. \nMultiattack: Claws a number of times equal to half the spell's level"
+	}],
+
+	features : [{
+		name : "Summon Aberration",
+		description : "You call forth an aberrant spirit. It manifests in an unoccupied space that you can see within range. This corporeal form uses the Aberrant Spirit stat block. When you cast the spell, choose a Beholderkin, Slaad, or Star Spawn. The creature resembles an aberration of that kind, which determines certain traits in its stat block.",
+		joinString : "\n   "
+	},{
+		name : "Higher Spell Level",
+		description : "The Aberrant Spirit stat block gains 1 AC, 10 HP, and 1 point of damage on each attack for each spell level above forth",
+		joinString : "\n   "
+	}],
+	actions : [{
+		name : "Multiattack",
+		description : "The aberration makes a number of attacks equal to half this spell's level (rounded down)",
+	}],
+	traits : [{
+		name : "Regeneration",
+		description : "The aberration regains 5 hit points at the start of its turn if it has at least 1 hit point."
+	}],
+}
+
+CreatureList["aberrant spirit star spawn"] = {
+
+	name : "Aberrant Spirit - Star Spawn",
+	source : ["TCoE", 109],
+	size : 3,
+	type : "Aberration",
+	header : "Summon",
+	companion : ["summon"],
+	companionApply : "summon",
+	alignment : "Unaligned",
+
+	ac : 11,
+	hp : 40,
+	hd : [4, 8],
+	speed : "30 ft",
+
+	proficiencyBonus : 0,
+	proficiencyBonusLinked : true,
+	challengeRating : "\"-\"",
+	scores : [16, 10, 15, 16, 10, 6],
+
+	senses : "Darkvision 60 ft",
+	languages : "Deep Speech, understands the languages you speak",
+	damage_immunities : "psychic",
+
+	attacksAction : 2,
+	attacks : [{
+		name : "Psychic Slam",
+		ability : 4,
+		useSpellcastingAbility : true,
+		abilitytodamage : true,
+		damage : [1, 8, "psychic"],
+		modifiers : ["", 4],
+		range : "Melee (5 ft)",
+		description : "Multiattack: Psychic Slam a number of times equal to half the spell's level"
+	}],
+
+	features : [{
+		name : "Summon Aberration",
+		description : "You call forth an aberrant spirit. It manifests in an unoccupied space that you can see within range. This corporeal form uses the Aberrant Spirit stat block. When you cast the spell, choose a Beholderkin, Slaad, or Star Spawn. The creature resembles an aberration of that kind, which determines certain traits in its stat block.",
+		joinString : "\n   "
+	},{
+		name : "Higher Spell Level",
+		description : "The Aberrant Spirit stat block gains 1 AC, 10 HP, and 1 point of damage on each attack for each spell level above forth",
+		joinString : "\n   "
+	}],
+	actions : [{
+		name : "Multiattack",
+		description : "The aberration makes a number of attacks equal to half this spell's level (rounded down)",
+	}],
+	traits : [{
+		name : "Whispering Aura",
+		description : "At the start of each of the aberrations turns, each creature within 5ft must succeed on a wisdom saving throw against your spell DC or take 2d6 psychic damage, provided the aberration isn't incapacitated.",
+	}],
+}
+
+CreatureList["celestial spirit defender"] = {
+
+	name : "Celestial Spirit - Defender",
+	source : ["TCoE", 110],
+	size : 2,
+	type : "Celestial",
+	header : "Summon",
+	companion : ["summon"],
+	companionApply : "summon",
+	alignment : "Unaligned",
+
+	ac : 13,
+	hp : 40,
+	hd : [4, 8],
+	speed : "30 ft, fly 40ft",
+
+	proficiencyBonus : 0,
+	proficiencyBonusLinked : true,
+	challengeRating : "\"-\"",
+	scores : [16, 14, 16, 10, 14, 16],
+
+	senses : "Darkvision 60 ft",
+	languages : "Celestial, understands the languages you speak",
+	damage_immunities : "radiant",
+	condition_immunities : "charmed, frightened",
+
+	attacksAction : 2,
+	attacks : [{
+		name : "Radiant Mace",
+		ability : 1,
+		useSpellcastingAbility : true,
+		abilitytodamage : true,
+		damage : [1, 10, "radiant"],
+		modifiers : ["", 5],
+		range : "Melee (5 ft)",
+		description : "Itself or another creature within 10 ft of the target gain 1d10 temporary hit points.\nMultiattack: Radiant Mace a number of times equal to half the spell's level"
+	}],
+
+	features : [{
+		name : "Summon Celestial",
+		description : "You call forth a celestial spirit. It manifests in an angelic form in an unoccupied space that you can see within range. This corporeal form uses the Celestial Spirit stat block. When you cast the spell, choose Avenger or Defender. Your choice determines the creature’s attack in its stat block. ",
+		joinString : "\n   "
+	},{
+		name : "Higher Spell Level",
+		description : "The Celestial Spirit stat block gains 1 AC, 10 HP, and 1 point of damage on each attack for each spell level above fifth",
+		joinString : "\n   "
+	}],
+	actions : [{
+		name : "Multiattack",
+		description : "The celestial makes a number of attacks equal to half this spell's level (rounded down)",
+	}],
+	traits : [{
+		name : "Healing Touch (1/day)",
+		description : "The celestial touches another creature. The target magically regains hit points equal to 2d8 + the spell’s level.",
+	}],
+}
+
+CreatureList["celestial spirit avenger"] = {
+
+	name : "Celestial Spirit - Avenger",
+	source : ["TCoE", 110],
+	size : 2,
+	type : "Celestial",
+	header : "Summon",
+	companion : ["summon"],
+	companionApply : "summon",
+	alignment : "Unaligned",
+
+	ac : 11,
+	hp : 40,
+	hd : [4, 8],
+	speed : "30 ft, fly 40ft",
+
+	proficiencyBonus : 0,
+	proficiencyBonusLinked : true,
+	challengeRating : "\"-\"",
+	scores : [16, 14, 16, 10, 14, 16],
+
+	senses : "Darkvision 60 ft",
+	languages : "Celestial, understands the languages you speak",
+	damage_immunities : "radiant",
+	condition_immunities : "charmed, frightened",
+
+	attacksAction : 2,
+	attacks : [{
+		name : "Radiant Bow",
+		ability : 2,
+		useSpellcastingAbility : true,
+		abilitytodamage : true,
+		damage : [2, 6, "radiant"],
+		modifiers : ["", 5],
+		range : "Range 150/600ft",
+		description : "Multiattack: Radiant Bow a number of times equal to half the spell's level"
+	}],
+
+	features : [{
+		name : "Summon Celestial",
+		description : "You call forth a celestial spirit. It manifests in an angelic form in an unoccupied space that you can see within range. This corporeal form uses the Celestial Spirit stat block. When you cast the spell, choose Avenger or Defender. Your choice determines the creature’s attack in its stat block. ",
+		joinString : "\n   "
+	},{
+		name : "Higher Spell Level",
+		description : "The Celestial Spirit stat block gains 1 AC, 10 HP, and 1 point of damage on each attack for each spell level above fifth",
+		joinString : "\n   "
+	}],
+	actions : [{
+		name : "Multiattack",
+		description : "The celestial makes a number of attacks equal to half this spell's level (rounded down)",
+	}],
+	traits : [{
+		name : "Healing Touch (1/day)",
+		description : "The celestial touches another creature. The target magically regains hit points equal to 2d8 + the spell’s level.",
+	}],
+}
+
+
+
 
 // Dysnomian
 CreatureList["testing-creature"] = {
