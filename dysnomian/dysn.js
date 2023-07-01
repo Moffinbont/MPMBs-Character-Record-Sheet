63984593cbfec35bd02fbb0158540c3658b0a179
Dysnomian Charles <dysnomian.gamer@gmail.com>
2023-07-01 13:48:17 +1200

    Use custom data for the different flavours of the monster.

--- dysnomian/dysn.js
+++ dysnomian/dysn.js
@@ -5,11 +5,15 @@ RequiredSheetVersion("13.1.7");
 (function (dysn) {
 	(function (helper) {
 		app.dysn.helper.PrintObj = function(obj, objName) {
-			if (objName) console.println(objName + ":");
+			
 			if (typeof obj === "string") {
-				console.println("\"" + obj + "\"");
+				console.println(objName + ": \"" + obj + "\"");
 			} else {
+				if (objName) console.println(objName + ":");
 				for (var prop in obj) {
+					if (!Object.prototype.hasOwnProperty.call(obj, prop)) {
+						continue;
+					}
 					if (prop === "eval" && obj["eval"]) { console.println("(eval: ...)"); }
 					else { console.println("(" + prop + ": " + obj[prop] + ")"); }
 				}
@@ -28,8 +32,81 @@ RequiredSheetVersion("13.1.7");
 				acPerSl: 1,
 				baseHp: 30,
 				hpPerAdditionalSl: 5,
+				defaultMonsterType: "Fey or Fiend",
+				monsterType: "Beast",
+				features: [],
+				actions: [],
+				traits: [],
+				flavours: {
+					land: {
+						regex: /land/i,
+						speed: "30 ft, climb 30 ft",
+						name: "Land",
+						traits: [{
+							name : "Pack Tactics",
+							description : "The beast has advantage on an attack roll against a creature if at least one of the hyena's allies is within 5 ft of the creature and the ally isn't incapacitated."
+						}],
+						// baseHp
+						// monsterType
+					},
+					water: {
+						regex: /water/i,
+						speed: "30 ft, swim 30 ft",
+						name: "Water",
+						traits: [{
+							name : "Pack Tactics",
+							description : "The beast has advantage on an attack roll against a creature if at least one of the hyena's allies is within 5 ft of the creature and the ally isn't incapacitated."
+						}, {
+							name : "Water Breathing",
+							description : "The beast can breathe only underwater."
+						}],
+					},
+					air: {
+						regex: /air/i,
+						speed: "30 ft, fly 30 ft",
+						name: "Air",
+						traits: [{
+							name : "Flyby",
+							description : "The beast doesn't provoke opportunity attacks when it flies out of an enemy's reach."
+						}],
+					}
+				}
 			}
 		}
+
+		app.dysn.summon.UpdateFormFields = function (prefix, summonInfo) {
+			// set creature type on the form directly - see functions2.js:498 for example of setting companion fields
+			Value(prefix + "Comp.Desc.MonsterType", summonInfo.monsterType);
+			// try {
+			// 	Value(prefix + "Comp.Race", summonInfo.race);
+			// } catch (err) {
+			// 	// do nothing, I'm not sure why the above line throws, maybe because the value we're setting the race to isn't in the dropdown list? But it still works so idk
+			// }
+			if (summonInfo.displaySpeed) Value(prefix + "Comp.Use.Speed", summonInfo.displaySpeed);
+			if (summonInfo.age) Value(prefix + "Comp.Desc.Age", summonInfo.age);
+			if (summonInfo.gender) Value(prefix + "Comp.Desc.Age", summonInfo.gender);		
+			if (summonInfo.hp) Value(prefix + "Comp.Use.HP.Max", summonInfo.hp);
+			if (summonInfo.sl) Value(prefix + "Comp.Use.HD.Level", summonInfo.sl);
+			if (summonInfo.ac) Value(prefix + "Comp.Use.AC", summonInfo.ac);
+			if (summonInfo.aPerA) Value(prefix + "Comp.Use.Attack.perAction", summonInfo.aPerA);
+		};
+
+
+		app.dysn.summon.GetDisplaySpeed = function (prefix, speed) {
+			console.println(speed);
+			var displaySpeed = What("Unit System") === "imperial" ? speed : ConvertToMetric(speed, 0.5);
+			if (typePF) { // this is a global var that is true when the sheet is the printer friendly one
+				// add line breaks for the printer friendly sheets
+				displaySpeed = displaySpeed.replace(/(,|;) /g, "$1\n");
+			}
+			console.println(displaySpeed);
+
+			var summonInfo = {
+				displaySpeed: displaySpeed,
+			}
+			return summonInfo;
+		};
+		
 	})(app.dysn.summon = app.dysn.summon || {});
 })(app.dysn = app.dysn || {});
 
@@ -78,7 +155,7 @@ RequiredSheetVersion("13.1.7");
 			name: "Bestial Spirit",
 			source: ["TCoE", 109],
 			size: 4,
-			type: ["Land", "Water", "Air"],
+			type: ["All", "Land", "Water", "Air"],
 			subtype: ["2nd level spell slot", "3rd level spell slot", "4th level spell slot", "5th level spell slot"],
 			header: "Summon",
 			companion: "summon-tashas",
@@ -115,93 +192,88 @@ RequiredSheetVersion("13.1.7");
 				
 			}],
 			traits: [{
-				name: "Pack Tactics",
+				name : "Flyby (Air Only)",
+				description : "The beast doesn't provoke opportunity attacks when it flies out of an enemy's reach."
+			}, {
+				name: "Pack Tactics (Land and Water Only)",
 				description: "The beast has advantage on an attack roll against a creature if at least one of the hyena's allies is within 5 ft of the creature and the ally isn't incapacitated."
+			}, {
+				name : "Water Breathing (Water Only)",
+				description : "The beast can breathe only underwater."
 			}],
 			
 			eval: function(prefix, lvl) {
 				console.println("processing eval");
 
+				// what is remembered currently? - "summon", it comes from the creature.companionApply, and is required for reloading? Maybe? TODO test changing this
+				//console.println("Remembered Type: " + What(prefix + "Comp.Type"));
+
 				// try looking here for the creature type set on the sheet, as chosen by the user
 				var inCompType = What(prefix + "Comp.Desc.MonsterType");
-				var speed;
-				var race;
-				var monsterType = "Beast";
-				if (/Air/i.test(inCompType)) {
-					console.println("processing air creature");
-					speed = "30 ft, fly 30 ft";
-					race = "Bestial Spirit (Air)";
-				} else if (/Water/i.test(inCompType)) {
-					console.println("processing water creature");
-					speed = "30 ft, swim 30 ft";
-					race = "Bestial Spirit (Water)";
-				} else {
-					console.println("processing land creature");
-					speed = "30 ft, climb 30 ft";		
-					race = "Bestial Spirit (Land)";
-				}
 				
-				console.println(speed);
-				var displaySpeed = What("Unit System") === "imperial" ? speed : ConvertToMetric(speed, 0.5);
-				if (typePF) { // this is a global var that is true when the sheet is the printer friendly one
-					// add line breaks for the printer friendly sheets
-					displaySpeed = displaySpeed.replace(/(,|;) /g, "$1\n");
-				}
-				console.println(displaySpeed);
-
-				// what is remembered currently? - "summon", it comes from the creature.companionApply, and is required for reloading? Maybe? TODO test changing this
-				console.println("Remembered Type: " + What(prefix + "Comp.Type"));
-				
-
-				// set creature type on the form directly - see functions2.js:498 for example of setting companion fields
-				Value(prefix + "Comp.Desc.MonsterType", monsterType);
-				try {
-					Value(prefix + "Comp.Race", race);
-				} catch (err) {
-					// do nothing, I'm not sure why the above line throws, maybe because the value we're setting the race to isn't in the dropdown list? But it still works so idk
-				}
-				Value(prefix + "Comp.Use.Speed", displaySpeed);
-
 				// find a digit, then either "st", "nd", "rd", or "th", then 0 or 1 whitespace chars, then "level", the i means case insensitive
 				var matchesSpellSlot = /\d(st|nd|rd|th)\s?level/i.exec(inCompType);
 				//app.dysn.helper.PrintObj(matchesSpellSlot, "matchesSpellSlot");
 
-				if (matchesSpellSlot.length > 0) Value(prefix + "Comp.Desc.Age", matchesSpellSlot[0]);
-
-				var strSpellSlot = matchesSpellSlot.length > 0 ? matchesSpellSlot[0] : "";
+				
+				var strSpellSlot = (matchesSpellSlot && matchesSpellSlot.length) > 0 ? matchesSpellSlot[0] : "";
 				app.dysn.helper.PrintObj(strSpellSlot, "strSpellSlot");
 
-				var sl = strSpellSlot.length > 0 ? +strSpellSlot[0] : 2	; // default SL is 2
+				var sl = strSpellSlot.length > 0 ? +strSpellSlot[0] : -1;
 				console.println("sl+" + sl);
-				app.dysn.helper.PrintObj(sl, "sl");
-
-				var creaNameRaw = How(prefix + "Comp.Race"); // How() finds .submitName
-				var creaName = ParseCreature(creaNameRaw);
-				// we don't even need to find the creature name, the current companion is saved to a global var 
-				// Actually the creature is seperate to the companion so do need creature name
+				//app.dysn.helper.PrintObj(sl, "sl");
 
 				var objComp = CurrentCompRace[prefix];
 				//dysn.helper.PrintObj(objComp, "objComp");
 
+				var creaNameRaw = How(prefix + "Comp.Race"); // How() finds .submitName
+				var creaName = ParseCreature(creaNameRaw);
+				// --we don't even need to find the creature name, the current companion is saved to a global var 
+				// Actually the creature is seperate to the companion so do need creature name
+
 				// Do we edit the creature object? Or do we have to edit the sheet fields directly?
 				var objCrea = CreatureList[creaName];
-				
-				
-				objCrea.type = "beast";
+				//dysn.helper.PrintObj(objComp, "objComp");
 
-				// Looks like we need to edit the sheet fields directly - maybe we can save our object somehow?
-				if (objCrea.summonData) {
-					var su = objCrea.summonData;
+				var su = objCrea.summonData; // Our custom property
+				app.dysn.helper.PrintObj(su, "summonData");
 
-					var hp;
+				var speed;
+				var gender;
+				var baseHp;
+				var monsterType;
+
+				var foundFlavour = false;
+				for (var prop in su.flavours) {
+					if (!Object.prototype.hasOwnProperty.call(su.flavours, prop)) {
+						continue;
+					}
+					//app.dysn.helper.PrintObj(prop, "prop");
+					var flavour = su.flavours[prop];
+					//app.dysn.helper.PrintObj(flavour, "flavour");
+
+					if (flavour.regex.test(inCompType)) {
+						console.println("processing " + flavour.name);
+						gender = flavour.name;
+						speed = flavour.speed ? flavour.speed : su.speed;
+						baseHp = flavour.baseHp ? flavour.baseHp : su.baseHp;
+						monsterType = flavour.monsterType ? flavour.monsterType : su.monsterType;
+						foundFlavour = true;
+						break;
+					}
+				}
+				
+				if (foundFlavour && sl !== -1) {					
+					var displaySpeed = app.dysn.summon.GetDisplaySpeed(prefix, speed);
+									var hp;
 					var ac;
 					var aPerA = sl / 2; // one attack for every two spell slot levels (round down).
-			
+
 					if (su.hpPerAdditionalSl && sl > su.lowestSl) {
 						// +5 hp for each sl above 2
-						hp = su.baseHp + (su.hpPerAdditionalSl * (sl - su.lowestSl));
+						hp = baseHp + (su.hpPerAdditionalSl * (sl - su.lowestSl));
 					} else {
-						hp = su.baseHp;
+						hp = baseHp;
 					}
 					
 					if (su.acPerSl) {
@@ -210,19 +282,27 @@ RequiredSheetVersion("13.1.7");
 						ac = au.baseAc;
 					}
 					
-					Value(prefix + "Comp.Use.HP.Max", hp);
-					Value(prefix + "Comp.Use.HD.Level", sl);
-					Value(prefix + "Comp.Use.AC", ac);
-					Value(prefix + "Comp.Use.Attack.perAction", aPerA);
-					
 
+					var summonInfo = {
+						displaySpeed: displaySpeed,
+						monsterType: monsterType,
+						hp: hp,
+						ac: ac,
+						aPerA: aPerA,
+						age: strSpellSlot,
+						gender: gender,
+					}
 
+					app.dysn.summon.UpdateFormFields(prefix, summonInfo)
 				} else {
-					console.println("No summonData.")
+					Value(prefix + "Comp.Desc.MonsterType", su.defaultMonsterType);
 				}
 				
 			},
-
+			removeeval : function(prefix, lvl) {
+				Value(prefix + "Comp.Desc.Age", "");
+				Value(prefix + "Comp.Desc.Gender", "");
+			},
 			summonData: dysn.summon.data.beast,
 
 		};
